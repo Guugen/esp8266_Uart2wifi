12a4fad14f972aae6b9cdbdd9f15efd73ae14311
Guugen <guugen@163.com>
2022-05-28 21:40:27 +0800

    main

--- main/main.c
+++ main/main.c
@@ -18,62 +18,45 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_event.h"
 #include "../head/station.h"
-#include "driver/uart.h"
-#include "../head/data_pool.h"
+#include "../head/uart.h"
+#include "../head/network.h"
 
-/**
- * This is an example which echos any data it receives on UART0 back to the sender,
- * with hardware flow control turned off. It does not use UART driver event queue.
- *
- * - Port: UART0
- * - Receive (Rx) buffer: on
- * - Transmit (Tx) buffer: off
- * - Flow control: off
- * - Event queue: off
- */
 
-#define BUF_SIZE (1024)
+extern char *TAG;
 
-static void echo_task()
-{
-    // Configure parameters of an UART driver,
-    // communication pins and install the driver
-    uart_config_t uart_config = {
-        .baud_rate = 74880,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
-    uart_param_config(UART_NUM_0, &uart_config);
-    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
-
-    // Configure a temporary buffer for the incoming data
-    Data_pool data_pool;
-    Data_pool_init(BUF_SIZE, &data_pool);
-    
-
-    while (1) {
-        // Read data from the UART
-        int len = uart_read_bytes(UART_NUM_0, data_pool.poolPtr, BUF_SIZE, 20 / portTICK_RATE_MS);
-        data_pool.last += len; 
-        // Write data back to the UART
-        uart_write_bytes(UART_NUM_0, (const char *) data_pool.poolPtr, data_pool.last);
-        data_pool.last = 0;
-    }
-}
-
-static void wifi_task()
-{
+static void wifi_task(){
+    //connect to AP		
     ESP_ERROR_CHECK(nvs_flash_init());
-
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-    wifi_init_sta();   
+    wifi_init_sta();
+    
+    //config tcp client
 }
+
 
 void app_main()
-{	
-    xTaskCreate(echo_task, "uart_echo_task", 1024, NULL, 10, NULL);
+{
+    int msg_id;
+
     wifi_task();
+    esp_mqtt_client_handle_t client = mqtt_client_init("mqtt://120.55.170.139:1883");
+    
+    while(1)
+    {
+       msg_id = esp_mqtt_client_publish(client, "/topic/test", "data_3", 0, 1, 0);
+       if(msg_id == -1) printf("publish fail");
+       else printf("publish success\r\n");
+       vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    
 }
+
+
+
+
+
+
+
+
